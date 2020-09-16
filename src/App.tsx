b3d90c028f90de586de0dd8db220c44eae7c1f8a
DHamZ <mochamzdaniel@gmail.com>
2020-09-16 15:29:40 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,170 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import './App.css';
 import { getRandomColor } from "./utils/color";
+import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import Chip from '@material-ui/core/Chip';
 
-// interface IFormProps {
-//   action: string;
-// }
+// Get Random Color
+const randCol = getRandomColor();
 
-// export interface IFormState {
-//   lum: string;
-// }
+// Set List Color for filter
+const listCol: string[] = [];
+randCol.map((color, i) => (
+  listCol.push(color[0].name)
+));
 
-// class Index extends React.Component<IFormProps, IFormState> {
-class Index extends React.Component {
-  // constructor(props: IFormProps) {
-  //   super(props);
-  //   this.state = { lum: '' };
-  // }
-  render() {
-    return (
-      <div>
-        <form className='row'>
-          {getRandomColor().map((color) => (
-            <div className='checkFilter'>
-              <label>
-                {color[0].name}
-              </label>
-              <input type="checkbox" name={color[0].name} checked />
-            </div>
-          ))}
-          {/* <select value={this.state.lum}>
-            <option value="">All</option>
+// Custom Styles
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 220,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  }),
+);
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name: string, colFil: string[], theme: Theme) {
+  return {
+    fontWeight:
+      colFil.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+const Index = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [randColor] = useState(randCol);
+  const [lum, setLum] = useState<string>('');
+  const [colFil, setColFil] = useState<string[]>([]);
+  console.log(randColor);
+  return (
+    <div>
+      <form className='row'>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-chip-label">Color Filter</InputLabel>
+          <Select
+            labelId="demo-mutiple-chip-label"
+            id="demo-mutiple-chip"
+            multiple
+            value={colFil}
+            onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
+              setColFil(event.target.value as string[]);
+            }}
+            input={<Input id="select-multiple-chip" />}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {(selected as string[]).map((value) => (
+                  <Chip key={value} label={value} className={classes.chip} />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {listCol.map((name) => (
+              <MenuItem key={name} value={name} style={getStyles(name, colFil, theme)}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.margin}>
+          <InputLabel htmlFor="demo-customized-select-native">Luminence</InputLabel>
+          <Select
+            native
+            value={lum}
+            onChange={(event: ChangeEvent<{ value: unknown }>) => {
+              setLum(event.target.value as string);
+            }}
+            inputProps={{
+              name: 'age',
+              id: 'age-native-simple',
+            }}
+          >
+            <option aria-label="All" value="" />
             <option value="darken">Darken</option>
             <option value="lighten">Lighten</option>
-          </select> */}
-        </form>
-        <div className='column'>
-          {getRandomColor().map((color) => (
-            <div className='row'>
-              {color.map((col) => (
-                <div className="box">
-                  <div className="box-fill" style={{ backgroundColor: JSON.parse(JSON.stringify(col)).hex }}></div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+          </Select>
+        </FormControl>
+      </form>
+      <div className='column'>
+        {randColor.map((color, i) => {
+          if (colFil.length < 1) {
+            return (
+              <div className='row' key={i}>
+                {color.map((col, j) => {
+                  if (lum === '') {
+                    return (
+                      <div className="box" key={i + '|' + j}>
+                        <div className="box-fill" style={{ backgroundColor: JSON.parse(JSON.stringify(col)).hex }}></div>
+                      </div>
+                    )
+                  } else if ((JSON.parse(JSON.stringify(col)).name !== 'darken' && JSON.parse(JSON.stringify(col)).name !== 'lighten') || JSON.parse(JSON.stringify(col)).name === lum) {
+                    return (
+                      <div className="box" key={i + '|' + j}>
+                        <div className="box-fill" style={{ backgroundColor: JSON.parse(JSON.stringify(col)).hex }}></div>
+                      </div>
+                    )
+                  }
+                })}
+              </div>
+            )
+          } else if (colFil.includes(color[0].name)) {
+            return (
+              <div className='row' key={i}>
+                {color.map((col, j) => {
+                  if (lum === '') {
+                    return (
+                      <div className="box" key={i + '|' + j}>
+                        <div className="box-fill" style={{ backgroundColor: JSON.parse(JSON.stringify(col)).hex }}></div>
+                      </div>
+                    )
+                  } else if ((JSON.parse(JSON.stringify(col)).name !== 'darken' && JSON.parse(JSON.stringify(col)).name !== 'lighten') || JSON.parse(JSON.stringify(col)).name === lum) {
+                    return (
+                      <div className="box" key={i + '|' + j}>
+                        <div className="box-fill" style={{ backgroundColor: JSON.parse(JSON.stringify(col)).hex }}></div>
+                      </div>
+                    )
+                  }
+                })}
+              </div>
+            )
+          }
+        })}
       </div>
-    )
-  }
+    </div >
+  )
 }
 export default Index;
--- src/utils/color.tsx
+++ src/utils/color.tsx
@@ -63,8 +63,8 @@ export function getRandomColor() {
         // get 5 column saturation colors
         satcol.push(pc[colorIndex]);
         for (var j = 0; j < 4; j++) {
-            const sl = Math.floor((Math.random() * 1));
-            const lumNum = Math.floor((Math.random() * 100) + 1) * (sl === 0 ? -1 : sl);
+            const sl = Math.floor((Math.random() * 2) + 1);
+            const lumNum = Math.floor((Math.random() * 20) + 1) * (sl < 2 ? -1 : sl);
             const newsatcol = {
                 name: lumNum < 0 ? 'darken' : 'lighten',
                 hex: colLum(pc[colorIndex].hex, lumNum),
@@ -81,7 +81,7 @@ function colLum(color: string, percent: number) {
     var num = parseInt(color.replace("#", ""), 16),
         amt = Math.round(2.55 * percent),
         R = (num >> 16) + amt,
-        B = (num >> 8 & 0x00FF) + amt,
+        B = (num >> 8 && 0x00FF) + amt,
         G = (num & 0x0000FF) + amt;
     return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
 };
